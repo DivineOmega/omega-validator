fed1abf84aef3bf96d36fe2ea36c868ad2f3b530
Jordan Hall <jordan@hall05.co.uk>
2019-03-17 00:16:07 +0000

    Document messages() output

--- test.php
+++ test.php
@@ -8,20 +8,35 @@ use DivineOmega\OmegaValidator\Validator;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$validator = new Validator([
+$data = [
     'email' => '',
-], [
+];
+
+$rules = [
     'email' => [
         new Required(),
         new IsEmail(),
         new IsString(),
-    ],
-]);
+    ]
+];
+
+$validator = new Validator($data, $rules);
 
 if ($validator->fails()) {
 
     // English messages
     var_dump($validator->messages());
+
+    // array(1) {
+    //  ["email"]=>
+    //  array(2) {
+    //    ["DivineOmega\OmegaValidator\Rules\Required"]=>
+    //    string(22) "The email is required."
+    //    ["DivineOmega\OmegaValidator\Rules\IsEmail"]=>
+    //    string(40) "The email must be a valid email address."
+    //  }
+    // }
+
 
     // German messages
     var_dump($validator->messages(new Translator('de')));
